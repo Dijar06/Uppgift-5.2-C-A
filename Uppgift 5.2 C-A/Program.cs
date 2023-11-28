199d0f516d92dbce1454fcd7a699bc6a75f9de8f
Dijar06 <dize06001@utb.vaxjo.se>
2023-11-28 14:59:07 +0100

    Add project files.

---
+++ Uppgift 5.2 C-A/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många heltal vill du skriva in?");
+            int x = int.Parse(Console.ReadLine());
+            int[] v = new int[x];
+            Console.WriteLine("Skriv in " + x + " heltal");
+            for (int i = 0; i < v.Length; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Här är  talen du skrev in");
+            for (int i = 0; i < v.Length; i++)
+            {
+                Console.WriteLine("Tal " + (i+1) + ": " + v[i]);
+            }
+        }
+    }
+}
